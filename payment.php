341059fb9b94bf90a555e9af660761a14c322369
daniel-cretney <61253525+daniel-cretney@users.noreply.github.com>
2020-07-29 23:31:17 -0400

    Add files via upload

---
+++ payment.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Chuber</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='css/main.css'>
+    <link rel='icon' href='photos/favicon.ico' type='image/x-icon'>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+</head>
+<nav>
+    <a href='index.html'><img id='image'src='photos/logo.jpg'></a>
+</nav>
+<body class='main-container'><br>
+    <h1 id='slogan'>Gettin' you where you need to be.</h1>
+    <form method='post' action='registration.php'>
+        <label id='slogan'>How long was your ride in minutes?</label><br>
+        <input type='number' name='number' min='1'required><br><br>
+        <input type='submit' id='button' class='btn btn-default' value='Submit'>
+    </form>
+</body>
+</html>

f4b304dc1d9cd94951708c88493468716292baa8
daniel-cretney <61253525+daniel-cretney@users.noreply.github.com>
2020-07-30 14:12:20 -0400

    Add files via upload

--- registration.php
+++ registration.php
@@ -6,7 +6,7 @@
     <title>Chuber</title>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
     <link rel='stylesheet' type='text/css' media='screen' href='css/main.css'>
-    <link rel='icon' href='photos/favicon.ico' type='image/x-icon'>
+    <link rel='icon' href='photos/favicon.png' type='image/x-icon'>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 </head>
 <nav>
@@ -15,7 +15,7 @@
 <body class='main-container'><br>
     <h1 id='slogan'>Choose Your Method of Payment</h1>
     <br><br>
-    <script src="https://www.paypal.com/sdk/js?client-id=AbyKot2-A1Di2DAL5y46CZcwazIVpDeBmVDKuuq5Yg2w5JTen-0qjz_xTXubpAGxj3s3dwkoHjwr7-Dj"> // Replace YOUR_SB_CLIENT_ID with your sandbox client ID
+    <script src="https://www.paypal.com/sdk/js?client-id=AbyKot2-A1Di2DAL5y46CZcwazIVpDeBmVDKuuq5Yg2w5JTen-0qjz_xTXubpAGxj3s3dwkoHjwr7-Dj"> 
     </script>
 
     <div id="paypal-button-container"></div>
